5c086e30053d1efaafdd45d2ad15da411c10b280
ajaychaudhary8800-ai <ajaychaudhary8800@gmail.com>
2026-09-25 13:05:11 +0530

    feat: track backup container startup

--- infra/backup/entrypoint.sh
+++ infra/backup/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
+date +%s > /tmp/backup-container-start
 echo "${BACKUP_CRON:-0 2 * * *} /usr/local/bin/backup.sh >> /proc/1/fd/1 2>> /proc/1/fd/2" > /etc/crontabs/root
 exec crond -f -l 2
